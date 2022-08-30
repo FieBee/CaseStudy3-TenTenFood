2e06ec79a4e455e301bd5b0b2bfbfe3b0c6f1f98
Hiep Nguyen <gagga5376@gmail.com>
2022-08-30 17:16:59 +0700

    fix implement iservice

--- src/main/java/com/example/tentententen/service/shop/IShopService.java
+++ src/main/java/com/example/tentententen/service/shop/IShopService.java
@@ -1,18 +1,12 @@
 package com.example.tentententen.service.shop;
 
 import com.example.tentententen.model.Shop;
+import com.example.tentententen.service.IService;
 
 import java.sql.SQLException;
 import java.util.List;
 
-public interface IShopService {
-    void addShop(Shop shop) throws SQLException;
+public interface IShopService extends IService<Shop> {
+    Shop selectShopByName(String name);
 
-    Shop selectShop(int id);
-
-    List<Shop> selectAllShops();
-
-    boolean deleteShop(int id)throws SQLException;
-
-    boolean updateShop(Shop shop)throws SQLException;
 }
