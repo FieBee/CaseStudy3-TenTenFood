b5a0efd8514134ab222028cb9cfce17451473bca
Hiep Nguyen <gagga5376@gmail.com>
2022-08-31 15:51:19 +0700

    xong crud bill_detail

--- src/main/java/com/example/tentententen/model/Bill_detail.java
+++ src/main/java/com/example/tentententen/model/Bill_detail.java
@@ -2,6 +2,7 @@ package com.example.tentententen.model;
 
 public class Bill_detail {
     private int bill_detail_id;
+    private int bill_id;
     private int item_id;
     private int quantity;
     private double price;
@@ -9,8 +10,9 @@ public class Bill_detail {
     public Bill_detail() {
     }
 
-    public Bill_detail(int bill_detail_id, int item_id, int quantity, double price) {
+    public Bill_detail(int bill_detail_id, int bill_id, int item_id, int quantity, double price) {
         this.bill_detail_id = bill_detail_id;
+        this.bill_id = bill_id;
         this.item_id = item_id;
         this.quantity = quantity;
         this.price = price;
@@ -22,6 +24,14 @@ public class Bill_detail {
 
     public void setBill_detail_id(int bill_detail_id) {
         this.bill_detail_id = bill_detail_id;
+    }
+
+    public int getBill_id() {
+        return bill_id;
+    }
+
+    public void setBill_id(int bill_id) {
+        this.bill_id = bill_id;
     }
 
     public int getItem_id() {
