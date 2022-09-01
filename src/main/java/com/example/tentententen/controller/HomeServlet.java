9d169b2186656135e07ff9a5dfdc89827c3a13ee
FieBee <phuonglf98@gmail.com>
2022-09-01 10:57:49 +0700

    add HomeServlet

---
+++ src/main/java/com/example/tentententen/controller/HomeServlet.java
@@ -0,0 +1,45 @@
+package com.example.tentententen.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="HomeServlet" , value = "/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String action = req.getParameter("action");
+       if (action == null){
+           action= "";
+       }
+       try{
+           switch (action){
+               case "ac":
+                   break;
+               default:
+                   showHome(req,resp);
+                   break;
+
+           }
+       } catch (ServletException e) {
+           throw new RuntimeException(e);
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+    }
+
+    private void showHome(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("client/home.html");
+        dispatcher.forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
